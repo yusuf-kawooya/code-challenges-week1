2bc3477327992a9759aa59ca6d975b6197f1dc7e
usuff999 <usuffkawooya@gmail.com>
2020-11-01 21:32:06 +0300

    code challenges for week1

---
+++ challenge3.js
@@ -0,0 +1,14 @@
+function productOfThreeNumbers(a,b,c) {
+    return a * b * c;
+}
+console.log(productOfThreeNumbers(1,2,3))
+
+function sumOfFive(d,e,f,g,h) {
+    return d + e + f + g + h;
+}
+console.log(sumOfFive(1,2,3,4,5));
+
+function divisionOftwo(i,j) {
+    return i/j;
+}
+console.log(divisionOftwo(2,4))
